84b16387bedc6455e3235583d0c6962db3dbedba
Patrick-052 <muiruripatrick052@gmail.com>
2023-05-28 16:30:56 +0300

    FizzBuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -19,9 +19,9 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0 and (i % 5) != 0:
+        elif (i % 3) == 0:
             tmp_result.append("Fizz")
-        elif (i % 5) == 0 and (i % 3) != 0:
+        elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
